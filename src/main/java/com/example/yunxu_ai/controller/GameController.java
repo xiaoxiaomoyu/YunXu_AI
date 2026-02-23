13486b4dd1c4d03bb59dec6780cab63727dfb273
xiaoxiaomoyu <2690052240@qq.com>
2026-02-23 19:45:35 +0800

    feat(simulator): 添加哄哄模拟器

---
+++ src/main/java/com/example/yunxu_ai/controller/GameController.java
@@ -0,0 +1,28 @@
+package com.example.yunxu_ai.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
+
+//哄哄模拟器
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/ai")
+public class GameController {
+
+    private final ChatClient gameChatClient;
+
+    @RequestMapping(value = "/game", produces = "text/html;charset=utf-8")
+    public Flux<String> chat(String prompt, String chatId) {
+        return gameChatClient.prompt()
+                .user(prompt)
+                .advisors(a -> a.param(CONVERSATION_ID, chatId))
+                .stream()
+                .content();
+    }
+}
